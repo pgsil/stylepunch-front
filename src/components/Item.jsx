8b30dc7dee14ef5ef8392d6f1f9d7809deb81deb
pgsil <email@email.com>
2018-06-23 18:20:56 -0300

    add description support

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-const Item = ({ author, image, title, url, isEnabled, addToList }) => (
+const Item = ({ author, image, title, url, isEnabled, addToList, description = null }) => (
   <article className="theme-item">
     <h3>{title}</h3>
 
     <img src={image} alt={`Screenshot of ${title}`} />
 
     <h4>by {author}</h4>
+
+    {description && <div className="description">{description}</div>}
+
     <a href={url}>Thread link</a>
 
     <button onClick={() => addToList()} className={isEnabled ? "btn btn-enabled" : "btn"}>
